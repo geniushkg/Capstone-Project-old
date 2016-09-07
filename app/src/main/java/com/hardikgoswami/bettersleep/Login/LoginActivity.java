39bbdf344ac4e6206271aa266e56175e59c87b70
geniushkg <geniushkg@gmail.com>
2016-09-07 20:03:09 +0530

    feat : authentication setup chores
    add firebase sdk for google+ athentication

---
+++ app/src/main/java/com/hardikgoswami/bettersleep/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.hardikgoswami.bettersleep.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hardikgoswami.bettersleep.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
