ddfd1d9b0545efb5623f460db108592da14d5ab2
geniushkg <geniushkg@gmail.com>
2016-09-14 07:20:20 +0530

    feat : add  toolbar , appbar layout
    implement appbar layout add toolbar

--- app/src/main/java/com/hardikgoswami/bettersleep/Dashboard/Dashboard.java
+++ app/src/main/java/com/hardikgoswami/bettersleep/Dashboard/Dashboard.java
@@ -1,18 +1,29 @@
 package com.hardikgoswami.bettersleep.Dashboard;
 
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.hardikgoswami.bettersleep.R;
 
 public class Dashboard extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // setup  toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
 }
