67ac685809f48fc7b17c591374a4c6bf74334477
geniushkg <geniushkg@gmail.com>
2016-09-23 12:24:28 +0530

    chore : update pager tab strip broken
    pager tab strip broken implment get title override but still not working

--- app/src/main/java/com/hardikgoswami/bettersleep/Dashboard/Dashboard.java
+++ app/src/main/java/com/hardikgoswami/bettersleep/Dashboard/Dashboard.java
@@ -2,7 +2,6 @@ package com.hardikgoswami.bettersleep.Dashboard;
 
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -20,14 +19,14 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_act);
-
         // setup  toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // setup viewpager
+        // setup viewpager & header strip
         ViewPager viewPager = (ViewPager) findViewById(R.id.vpPager);
         pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pagerAdapter);
         Log.d(TAG, "Dashboard activity");
+
     }
 
 
