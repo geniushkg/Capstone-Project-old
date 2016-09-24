55bb29a4c956dea6625ccb313b61b43198869b3b
geniushkg <geniushkg@gmail.com>
2016-09-24 12:17:14 +0530

    feat : implement youtube player
    embed youtube player inside frame layout working decent

--- app/src/main/java/com/hardikgoswami/bettersleep/yognidra/YognidraFragment.java
+++ app/src/main/java/com/hardikgoswami/bettersleep/yognidra/YognidraFragment.java
@@ -3,11 +3,17 @@ package com.hardikgoswami.bettersleep.Yognidra;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.hardikgoswami.bettersleep.R;
 
 /**
@@ -22,8 +28,10 @@ public class YognidraFragment extends Fragment {
 
     private String title;
     private int page;
+    private static final String API_KEY = "AIzaSyDOb-g_CBP0oo1T-0EpFprYaNYMHbsWWDs";
+    private static final String VIDEO_ID = "Mkx88_3cD-s";
 
-    public static YognidraFragment newInstance (int page , String title){
+    public static YognidraFragment newInstance(int page, String title) {
         YognidraFragment yognidraFragment = new YognidraFragment();
         Bundle args = new Bundle();
         args.putInt("someInt", page);
@@ -45,9 +53,33 @@ public class YognidraFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView =inflater.inflate(R.layout.yognidra_frag, container, false);
-        TextView tvLabel = (TextView) rootView.findViewById(R.id.tvSampleText);
-        tvLabel.setText(page + " -- " + title);
+        View rootView = inflater.inflate(R.layout.yognidra_frag, container, false);
+        // setup youtube
+        YouTubePlayerSupportFragment youTubePlayerSupportFragment = YouTubePlayerSupportFragment.newInstance();
+
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.youtube_layout, youTubePlayerSupportFragment).commit();
+
+
+        youTubePlayerSupportFragment.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean b) {
+                if (!b) {
+                    player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
+                    player.loadVideo(VIDEO_ID);
+                    player.play();
+                }
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult result) {
+                String errorMessage = result.toString();
+                Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_LONG).show();
+                Log.d("youtube error : ", errorMessage);
+            }
+        });
+
+
         return rootView;
     }
 
