b253993c289b6471ad3a9dc0f2704b4ed3922b18
geniushkg <geniushkg@gmail.com>
2016-10-01 16:52:14 +0530

    chore : update git and build scripts

--- app/src/main/java/com/hardikgoswami/bettersleep/Dashboard/Dashboard.java
+++ app/src/main/java/com/hardikgoswami/bettersleep/Dashboard/Dashboard.java
@@ -3,25 +3,26 @@ package com.hardikgoswami.bettersleep.Dashboard;
 import android.os.Handler;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.astuetz.PagerSlidingTabStrip;
 import com.hardikgoswami.bettersleep.Data.Source.SleepDataRepository;
 import com.hardikgoswami.bettersleep.R;
 import com.hardikgoswami.bettersleep.SleepPunch.SleepPunchContract;
-import com.hardikgoswami.bettersleep.SleepPunch.SleepPunchPresenter;
 
-public class Dashboard extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+
+public class Dashboard extends AppCompatActivity  implements {
     public static final String TAG = "BETTERSLEEP";
     private Boolean exit = false;
     ViewPagerAdapter pagerAdapter;
     LoaderManager loaderManager;
     SleepDataRepository dataRepository;
     SleepPunchContract.Presenter presenter;
+    SimpleOnPageChangeListener simpleOnPageChangeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +34,20 @@ public class Dashboard extends AppCompatActivity implements ViewPager.OnPageChan
         ViewPager viewPager = (ViewPager) findViewById(R.id.vpPager);
         pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pagerAdapter);
-        viewPager.setOnPageChangeListener(this);
         PagerSlidingTabStrip pagerSlidingTabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         pagerSlidingTabStrip.setViewPager(viewPager);
+        // setup SimpleOnPageListener
+        simpleOnPageChangeListener = new SimpleOnPageChangeListener();
+
         Log.d(TAG, "Dashboard activity");
         // setup loadermanager
         loaderManager = getSupportLoaderManager();
         // setup SleepData Repository
         dataRepository = new SleepDataRepository(this);
         // TODO: Setup Presenter for sleeppunch
+
+
+
     }
 
 
@@ -64,18 +70,5 @@ public class Dashboard extends AppCompatActivity implements ViewPager.OnPageChan
 
     }
 
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-        Toast.makeText(this,"pos is :"+position,Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
-    }
 }
