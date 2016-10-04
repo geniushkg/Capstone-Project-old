0517dc2218e99d285d26099f43694796b73f8cb1
geniushkg <geniushkg@gmail.com>
2016-10-04 19:34:39 +0530

    feat : sleep pattern loader implement

---
+++ app/src/main/java/com/hardikgoswami/bettersleep/Data/Source/DebtPatternLoader.java
@@ -0,0 +1,39 @@
+package com.hardikgoswami.bettersleep.Data.Source;
+
+import android.content.AsyncTaskLoader;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+
+import com.hardikgoswami.bettersleep.Data.Source.Local.Debt;
+import com.hardikgoswami.bettersleep.Data.Source.Local.SleepDebtContract;
+
+/**
+ * Created by geniushkg on 9/28/2016.
+ */
+public class DebtPatternLoader extends AsyncTaskLoader<Debt> {
+	// TODO: to update class for use in sleep pattern fragment
+    Debt mDebt;
+    public DebtTimeLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    public Debt loadInBackground() {
+        ContentResolver resolver = getContext().getContentResolver();
+        String[] projection = new String[]{SleepDebtContract.INT_DEBT_HOURS};
+        Cursor cursor =
+                resolver.query(SleepDebtContract.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        null);
+        int currentDebt = 0;
+        if(cursor != null){
+            currentDebt = cursor.getInt(0);
+        }
+        mDebt = new Debt(currentDebt);
+        return mDebt;
+    }
+
+}
