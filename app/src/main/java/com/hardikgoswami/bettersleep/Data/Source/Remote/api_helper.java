474124c3960bf170a7d6c7f7c70da72b70d5e1d6
geniushkg <geniushkg@gmail.com>
2016-10-07 17:21:22 +0530

    feat : concrete class for remote repo

--- app/src/main/java/com/hardikgoswami/bettersleep/Data/Source/Remote/api_helper.java
+++ app/src/main/java/com/hardikgoswami/bettersleep/Data/Source/Remote/api_helper.java
@@ -1,8 +1,21 @@
 package com.hardikgoswami.bettersleep.Data.Source.Remote;
 
 
-//todo : add retrofit instance and method to push data to php api  backend
+//// TODO: 10/7/2016 add firebase instance and helper to update data to web and fetch data from web
 
 public class api_helper{
+    int debtHour;
+    int userId;
 
+    public void newUserWithId(int userId , int debtHour){
+
+    }
+
+    public boolean checkIfUserExist(int userId){
+        return false;
+    }
+
+    public int getUserDebtFromId(int userId){
+    return 0;
+    }
 }
