c7e8f26f5fcb86dffd8f48556c7d9e45790e18ce
geniushkg <geniushkg@gmail.com>
2016-10-09 22:23:04 +0530

    feat : check user

--- app/src/main/java/com/hardikgoswami/bettersleep/Data/Source/Remote/api_helper.java
+++ app/src/main/java/com/hardikgoswami/bettersleep/Data/Source/Remote/api_helper.java
@@ -13,16 +13,41 @@ import com.google.firebase.database.ValueEventListener;
 
 public class api_helper{
     public static final String TAG = "BETTERSLEEP";
+    public static boolean status = false;
     public static void newUserWithId(int userId , int debtHour){
 
     }
 
     public static boolean checkIfUserExist(int userId){
-        return false;
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(""+userId);
+        // Read from the database
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                if(value != null) {
+                    Log.d(TAG, "Value is: " + value);
+                    status =true;
+                }else {
+                    Log.d(TAG,"null value or refrence not found");
+                    status = false;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG,"failure :"+databaseError.getMessage());
+            }
+        });
+        return status;
+
     }
 
     public static int getUserDebtFromId(int userId){
     return 0;
     }
-// TODO: 10/8/2016 implement sync adapter and complete firebase integration 
+// TODO: 10/8/2016 implement sync adapter and complete firebase integration
 }
