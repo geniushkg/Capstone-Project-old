bda7bdffc40c65bda62c0cf72ed177764dbffccf
geniushkg <geniushkg@gmail.com>
2016-10-15 21:51:18 +0530

    feat : sleep debt widget

---
+++ app/src/main/java/com/hardikgoswami/bettersleep/Widget/SleepDebtWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.hardikgoswami.bettersleep.Widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by geniushkg on 10/15/2016.
+ */
+
+public class SleepDebtWidgetProvider extends AppWidgetProvider {
+}
