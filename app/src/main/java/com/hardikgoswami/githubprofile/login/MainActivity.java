36be97829f4465d57cc716a1f0f9a0ff47fe9893
geniushkg <geniushkg@gmail.com>
2016-10-25 16:06:12 +0530

    feat : firebase setup

---
+++ app/src/main/java/com/hardikgoswami/githubprofile/login/MainActivity.java
@@ -0,0 +1,42 @@
+package com.hardikgoswami.githubprofile.login;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.hardikgoswami.githubprofile.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "GITHUBPROFILE" ;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // firebase helper and listener
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+    }
+}
