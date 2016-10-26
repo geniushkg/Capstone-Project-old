c48b6cf80ad204dd3032236ce2b774873f1aef62
geniushkg <geniushkg@gmail.com>
2016-10-26 11:52:20 +0530

    bug : oauth screen redirection

--- app/src/main/java/com/hardikgoswami/githubprofile/login/LoginActivity.java
+++ app/src/main/java/com/hardikgoswami/githubprofile/login/LoginActivity.java
@@ -1,22 +1,37 @@
 package com.hardikgoswami.githubprofile.login;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebView;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.hardikgoswami.githubprofile.BuildConfig;
 import com.hardikgoswami.githubprofile.R;
+import com.hardikgoswami.githubprofile.login.oauth.OauthFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "GITHUBPROFILE";
+    private static final String OAUTH_URL = "https://github.com/login/oauth/authorize";
+    private static final String CLIENT_ID = BuildConfig.CLIENT_ID;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    public static String url = OAUTH_URL + "?client_id=" + CLIENT_ID ;;
 
+    private Button mLoginButton;
+    private Dialog mAuthDialog;
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,17 @@ public class LoginActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.login_act);
         getSupportActionBar().hide();
+ 
+        //setup button
+        mLoginButton = (Button) findViewById(R.id.login_btn);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // start login dialog
+                showDialog();
+            }
+        });
+
         // firebase helper and listener
         mAuth = FirebaseAuth.getInstance();
 
@@ -35,6 +61,7 @@ public class LoginActivity extends AppCompatActivity {
                 if (user != null) {
                     // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    // navigate to home activity
                 } else {
                     // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
@@ -44,6 +71,25 @@ public class LoginActivity extends AppCompatActivity {
 
         };
 
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Uri uri = intent.getData();
+        System.out.println("URI=" + uri);
+        Log.d(TAG,"uri data is : "+ uri);
+        //// TODO: 10/26/2016 Authenticate with firebase and use code for getting token 
+    }
+
+    //Display the oAuth web page in a dialog
+    void showDialog() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.addToBackStack(null);
+
+        // Create and show the dialog.
+        OauthFragment newFragment = new OauthFragment();
+        newFragment.show(ft, "dialog");
     }
 
     @Override
