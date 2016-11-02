816f1336bdea5034186e29f39ff186c802e6ded8
geniushkg <geniushkg@gmail.com>
2016-11-02 10:25:07 +0530

    feat : Oauth token save to share preference

--- app/src/main/java/com/hardikgoswami/githubmetrics/Login/LoginActivity.java
+++ app/src/main/java/com/hardikgoswami/githubmetrics/Login/LoginActivity.java
@@ -1,8 +1,7 @@
 package com.hardikgoswami.githubmetrics.Login;
 
-import android.graphics.Bitmap;
-import android.opengl.Visibility;
-import android.os.AsyncTask;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,21 +10,15 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.hardikgoswami.githubmetrics.BuildConfig;
+import com.hardikgoswami.githubmetrics.Home.HomeActivity;
 import com.hardikgoswami.githubmetrics.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -61,73 +54,76 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         setContentView(R.layout.webview);
         progresview = (com.rey.material.widget.ProgressView) findViewById(R.id.progress_view);
-        progresview.setVisibility(View.GONE);
+        progresview.setVisibility(View.VISIBLE);
         webview = (WebView) findViewById(R.id.webview);
         tv_web = (TextView) findViewById(R.id.tv_webview);
         webview.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 super.shouldOverrideUrlLoading(view, url);
-
                 CODE = url.substring(url.lastIndexOf("?code=") + 1);
-                tv_web.setVisibility(View.VISIBLE);
-                tv_web.setText("Loading please wait");
-                String[] code = url.split("code=");
-                codeFromGithub(code[1]);
+                Log.d(TAG,"code fetched is :"+CODE);
+                String[] token_code = CODE.split("=");
+                fetchOauthTokenWithCode(token_code[1]);
                 return false;
             }
         });
         String url_load = GITHUB_URL + "?client_id=" + CLIENT_ID;
         webview.loadUrl(url_load);
-
     }
 
-    private void codeFromGithub(String s) {
-        progresview.setVisibility(View.VISIBLE);
-        Log.d(TAG, "code is:" + s);
-        webview.setVisibility(View.INVISIBLE);
-        tv_web.setText("Fetching token");
-        fetchOauthToken(s);
-    }
-
-    private void fetchOauthToken(String s) {
-        OkHttpClient client = new OkHttpClient();
-        HttpUrl.Builder url = HttpUrl.parse(GITHUB_OAUTH).newBuilder();
-        url.addQueryParameter("client_id", CLIENT_ID);
-        url.addQueryParameter("client_secret", CLIENT_SECRET);
-        url.addQueryParameter("code", s);
-        String url_oauth = url.build().toString();
-        final Request request = new Request.Builder()
-                .header("Accept", "application/json")
-                .url(url_oauth)
-                .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if(response.isSuccessful()){
-                    String JsonData = response.body().string();
-                    try {
-                       JSONObject jsonObject = new JSONObject(JsonData);
-                        String auth_token = jsonObject.getString("access_token");
-                        progresview.setVisibility(View.INVISIBLE);
-                        tv_web.setText("Token found : "+auth_token);
-                    }catch (JSONException exp){
-                        Log.d(TAG,"json exception :"+exp.getMessage());
-                    }
+    private void fetchOauthTokenWithCode(String code) {
+            OkHttpClient client = new OkHttpClient();
+            HttpUrl.Builder url = HttpUrl.parse(GITHUB_OAUTH).newBuilder();
+            url.addQueryParameter("client_id", CLIENT_ID);
+            url.addQueryParameter("client_secret", CLIENT_SECRET);
+            url.addQueryParameter("code", code);
+            String url_oauth = url.build().toString();
+            final Request request = new Request.Builder()
+                    .header("Accept", "application/json")
+                    .url(url_oauth)
+                    .build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
 
                 }
 
-            }
-        });
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if(response.isSuccessful()){
+                        String JsonData = response.body().string();
+                        Log.d(TAG,"response is:"+JsonData);
+                        try {
+                            JSONObject jsonObject = new JSONObject(JsonData);
+                            String auth_token = jsonObject.getString("access_token");
+                           // progresview.setVisibility(View.INVISIBLE);
+                            //tv_web.setText("Token found : "+auth_token);
+                            Log.d(TAG,"token is :"+auth_token);
+                            storeToSharedPreference(auth_token);
+                        }catch (JSONException exp){
+                            Log.d(TAG,"json exception :"+exp.getMessage());
+                        }
+
+                    }
+
+                }
+            });
+
+
     }
 
-    public static void storeTokenAndAuthenticateWithFirebase(String token){
-
+    private void storeToSharedPreference(String auth_token) {
+        SharedPreferences prefs = getSharedPreferences("github_prefs",MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putString("oauth_token",auth_token);
+        edit.commit();
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
+
 
 }
