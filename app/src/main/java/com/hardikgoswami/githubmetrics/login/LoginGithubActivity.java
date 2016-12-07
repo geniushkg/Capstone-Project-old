d6b7b193a6b8e76fbba5f72c7b2a9600e537f85c
geniushkg <geniushkg@gmail.com>
2016-12-07 18:35:46 +0530

    feat : add firebase config , homeactivity

--- app/src/main/java/com/hardikgoswami/githubmetrics/login/LoginGithubActivity.java
+++ app/src/main/java/com/hardikgoswami/githubmetrics/login/LoginGithubActivity.java
@@ -1,17 +1,19 @@
 package com.hardikgoswami.githubmetrics.login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.hardikgoswami.githubmetrics.BuildConfig;
 import com.hardikgoswami.githubmetrics.R;
-import com.hardikgoswami.oauthLibGithub.GithubOauth;
 
 public class LoginGithubActivity extends AppCompatActivity {
 
+    public static final String GIT_CLIENT = BuildConfig.GITHUB_CLIENT;
+    public static final String GIT_SECRET = BuildConfig.GITHUB_SECRET;
     Button loginBtn;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +22,7 @@ public class LoginGithubActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                
             }
         });
     }
