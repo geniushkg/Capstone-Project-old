7153e2e40a7f0507dae0762db01190c67f27ee8a
geniushkg <geniushkg@gmail.com>
2016-12-08 18:13:04 +0530

    feat : github contribution view streaks

--- app/src/main/java/com/hardikgoswami/githubmetrics/search/SearchFragment.java
+++ app/src/main/java/com/hardikgoswami/githubmetrics/search/SearchFragment.java
@@ -3,9 +3,15 @@ package com.hardikgoswami.githubmetrics.search;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.text.TextDirectionHeuristicCompat;
+import android.support.v4.widget.TextViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hardikgoswami.githubmetrics.R;
 
@@ -20,11 +26,21 @@ public class SearchFragment extends Fragment {
     }
 
 
+    EditText etGithubUsername;
+    Button btnSearch;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        etGithubUsername = (EditText) rootView.findViewById(R.id.etGithubUser);
+        btnSearch = (Button) rootView.findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Searched",Toast.LENGTH_SHORT);
+            }
+        });
 
         return rootView;
     }
