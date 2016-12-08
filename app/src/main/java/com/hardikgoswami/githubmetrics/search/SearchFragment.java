66e2e83486392edbc87c6eb4525dd7db847b6b95
geniushkg <geniushkg@gmail.com>
2016-12-08 21:37:45 +0530

    feat : github contribution view added

--- app/src/main/java/com/hardikgoswami/githubmetrics/search/SearchFragment.java
+++ app/src/main/java/com/hardikgoswami/githubmetrics/search/SearchFragment.java
@@ -10,9 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.javierugarte.GitHubContributionsView;
 import com.hardikgoswami.githubmetrics.R;
 
 /**
@@ -26,21 +28,37 @@ public class SearchFragment extends Fragment {
     }
 
 
+    GitHubContributionsView gitView;
     EditText etGithubUsername;
     Button btnSearch;
+    RatingBar ratingBar;
+    String searchedUser;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_search, container, false);
         etGithubUsername = (EditText) rootView.findViewById(R.id.etGithubUser);
+        gitView = (GitHubContributionsView)rootView.findViewById(R.id.github_contributions_view);
         btnSearch = (Button) rootView.findViewById(R.id.btnSearch);
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(),"Searched",Toast.LENGTH_SHORT);
+
+                if (etGithubUsername.getText().toString() != null) {
+                    searchedUser = etGithubUsername.getText().toString();
+                }
+                if (searchedUser != null) {
+                    Toast.makeText(getActivity(), "Seached is " + searchedUser, Toast.LENGTH_SHORT).show();
+                    gitView.loadUserName(searchedUser);
+                    gitView.displayMonth(true);
+                    gitView.setTextColor(R.color.colorPrimary);
+                }
+                Toast.makeText(getContext(), "Searched", Toast.LENGTH_SHORT);
             }
         });
+
 
         return rootView;
     }
